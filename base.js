cbd77a45b9374afcd08f79d0c8e997d54331a2e1
nagisc007 <nagisc007@yahoo.co.jp>
2021-06-05 17:24:05 +0900

    Changed: table create

--- base.js
+++ base.js
@@ -1,14 +1,7 @@
-const data_url = 'novels.json';
-
-function ShowContentsList(jsonObj){
-    const data = jsonObj.results[0]
-    console.log(data);
-}
+const data_url = '.novels.json';
 
 window.addEventListener('load', () => {
-    fetch(data_url, {
-        mode: 'cors'
-    })
+    fetch(data_url)
     .then((response) => {
         return response.json()
     })
@@ -21,21 +14,46 @@ window.addEventListener('load', () => {
     console.log('hello');
 })
 
-function TestContentsList(){
-    const table = document.createElement('div');
-    table.classList.add('table');
-    table.textContent = 'タイトル';
-    document.body.appendChild(table);
+function ShowContentsList(jsonObj){
+    const data = jsonObj.results[0]
+    console.log(data);
+}
 
-    for (let i = 0; i < 4; i++) {
-        const dl = document.createElement('dl');
-        dl.classList.add('row');
-        table.appendChild(dl);
-        for (let j = 0; j < 4; j++) {
-            const dt = document.createElement('dt');
-            dt.classList.add('cell');
-            dt.textContent = `${i}行${j}列`;
-            dl.appendChild(dt);
-        }
+const TestJson = {
+    'contents': [
+        {'index': 0, 'title': 'TestA'},
+        {'index': 1, 'title': 'TestB'}
+    ]
+}
+
+function TestContentsList(){
+    ShowTableFromJson(TestJson);
+}
+
+function ShowTableFromJson(obj){
+    // データ取得
+    const data = obj.contents;
+    // 要素作成
+    const table = document.createElement('table');
+    // ヘッダー作成
+    const tr = document.createElement('tr');
+    for (key in data) {
+        const th = document.createElement('th');
+        th.classList.add('row');
+        th.textContent = key;
+        tr.appendChild(th);
     }
+    table.appendChild(tr);
+    // ボディ作成
+    for (let i = 0; i < data.length; i++) {
+        const tr = document.createElement('tr');
+        for (key in data) {
+            const td = document.createElement('td');
+            td.textContent = data[i][key];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    // table追加
+    document.body.appendChild(table);
 }
